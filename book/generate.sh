28ae1811b0eeac2ac8925e844ab7a3ff67ab5550
Tomoyuki Mano <tomoyukimano@gmail.com>
2020-05-24 18:02:53 +0900

    fixed generate.sh; now it produces index.html instead of main.html

--- book/generate.sh
+++ book/generate.sh
@@ -1,7 +1,7 @@
 file=$1
 
 echo generating html... "${file%.*}"
-bundler exec asciidoctor -o "dist/${file%.*}.html" $file
+bundler exec asciidoctor -o "dist/index.html" $file
 cp -r imgs dist/
 
 echo Green generating pdf by asciidoctor-pdf with thema ...
